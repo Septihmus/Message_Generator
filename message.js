85764add9b64699a595dfe78a1a43c6b744a373b
Septihmus <81243705+Septihmus@users.noreply.github.com>
2021-03-25 17:14:53 +0000

    Update Messga.js

--- message.js
+++ message.js
@@ -1,3 +1,6 @@
+
+const randomMessage = () =>{
+
 const m1 = ['Time to','Do not','Would be great to'];
 const m2 = ['go for a drink','take a nap','open the window'];
 const m3 = ['because It\'s 1:32pm.','because It\'s 9:52am.','because It\'s noon.'];
@@ -11,3 +14,6 @@ const rNumber = (arr) => {
 
 
 console.log(rNumber(m1),rNumber(m2),rNumber(m3));
+};
+
+randomMessage();
